1a5fc2f95c40ebd50ddc00a1509f1ce7017d50ac
Nodirbek <berdimurodovnodir89@gmail.com>
2026-04-13 13:06:44 +0500

    done

--- app/api/leads.py
+++ app/api/leads.py
@@ -1,27 +1,3 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.sql import func
-
-from app.db.base import Base
-
-
-class Lead(Base):
-    __tablename__ = "leads"
-
-    id = Column(Integer, primary_key=True, index=True)
-
-    name = Column(String, nullable=False)
-    phone = Column(String, index=True)
-
-    status = Column(String, default="new")
-
-    manager_id = Column(Integer, ForeignKey("users.id"))
-
-    source = Column(String)
-    tag = Column(String)
-
-    created_at = Column(DateTime, server_default=func.now())
-
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
@@ -92,3 +68,11 @@ def delete_lead(lead_id: int, db: Session = Depends(get_db)):
     db.commit()
 
     return {"message": "Lead deleted"}
+
+
+@router.get("/search/")
+def search_leads(query: str, db: Session = Depends(get_db)):
+
+    leads = db.query(Lead).filter(Lead.name.ilike(f"%{query}%")).all()
+
+    return leads
--- app/main.py
+++ app/main.py
@@ -1,23 +1,12 @@
 from fastapi import FastAPI
-from fastapi import FastAPI
-from app.db.base import Base
-from app.db.session import engine
+
 from app.api.auth import router as auth_router
-
-
-app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return {"message": "CRM backend working"}
-
+from app.api.leads import router as leads_router
 
 app = FastAPI(title="CRM API")
 
-Base.metadata.create_all(bind=engine)
-
 app.include_router(auth_router)
+app.include_router(leads_router)
 
 
 @app.get("/")
